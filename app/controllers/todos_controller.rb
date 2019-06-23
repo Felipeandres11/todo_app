37974a871e6cd44f3a187d60792e8d192998e629
Felipe <felipeandres_uc@outlook.com>
2019-06-23 18:10:23 -0400

    Add validate

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,44 +1,32 @@
 class TodosController < ApplicationController
 
     def index
-        @todos = Todo.all 
+        @todos = Todo.all
     end
 
-    def new 
+    def new
         @todo = Todo.new
     end
 
 
-    def create 
-
-        #En el controller todos crear el método create. Este método debe crear un nuevo ToDo a partir de
-        #los parámetros recibidos del formulario bajo el scope de ToDo (params[:todo]).
-        #Filtrar los parámetros recibidos bajo el scope de ToDos en un método privado llamado
-        #todo_params para poder utilizar asignación masiva.
-        #El método create después de almacenar el todo debe redireccionar al index de ToDos.
-
+    def create
         @todo_Params = Todo.new(todo_params)
-
         @todo_Params.save
-
         redirect_to todos_path
-
-
     end
 
+    def lista
+      
+        @todos = Todo.all
+
+    end
 
     def show
-        #En el controller todos crear el método show. Este método debe buscar un ToDo en base al
-        #parámetro 'id'. (Todo.find) y asignarlo a a la variable @todo
-        #Crear la vista show.html.erb
-        #La vista debe mostrar la descripción del ToDo y si se encuentra completado o no.
-        #En la vista Index: agregar un link a Show a cada ToDo listado.
-       
+
         @todo = Todo.find(params[:id])
- 
     end
 
-    def edit 
+    def edit
         @todo = Todo.find(params[:id])
     end
 
@@ -48,11 +36,29 @@ class TodosController < ApplicationController
         redirect_to todos_path
     end
 
-    
+    def destroy
+        @todo = Todo.find(params[:id])
+        @todo.destroy
+        redirect_to todos_path
+    end
+
+
+
+    def complete
+
+          @todo = Todo.find(params[:id])
+          #@todo = Todo.update(params[:completed])
+          @todo.completed = true
+            #if @todo.completed == false
+            #    @todo.completed == true
+            #end
+          @todo.save
+          redirect_to todos_path
+    end
+
     private
     def todo_params
         params.require(:todo).permit(:description, :completed)
     end
 
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-
+  get '/todos/lista', to: 'todos#lista'
   resources :todos
+  #delete 'todos/:id', to: 'todos#destroy'
+  get '/todos', to: 'todos#index'
+  get '/todos/new', to: 'todos#new'
+  get '/todos/:id/edit', to: 'todos#edit', as: 'edit'
+  patch '/todos/:id', to: 'todos#update'
+  post '/todos/create', to: 'todos#create'
+  delete '/todos/:id', to: 'todos#destroy'
+
+  get '/todos/:id', to: 'todos#show'
+  get '/todos/:id/complete', to: 'todos#complete', as: 'complete'
+
+
 end
